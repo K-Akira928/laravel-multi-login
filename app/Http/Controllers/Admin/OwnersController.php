296797a7cbe6f6adca3644cedd791f18ccbb26f9
K-Akira928 <K.AkiraRei928@gmail.com>
2024-04-27 21:02:48 +0900

    feat: オーナー登録処理の実装

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Owner;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
 
 class OwnersController extends Controller
 {
@@ -36,7 +38,19 @@ class OwnersController extends Controller
    */
   public function store(Request $request)
   {
-    //
+    $request->validate([
+      'name' => ['required', 'string', 'max:255'],
+      'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . Owner::class],
+      'password' => ['required', 'confirmed', Rules\Password::defaults()],
+    ]);
+
+    Owner::create([
+      'name' => $request->name,
+      'email' => $request->email,
+      'password' => Hash::make($request->password),
+    ]);
+
+    return redirect()->route('admin.owners.index')->with('message', "オーナー登録が完了しました");
   }
 
   /**
