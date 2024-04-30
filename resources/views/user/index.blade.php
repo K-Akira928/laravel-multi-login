c2e02d437e5d3a48bb82219f1159f7f61151d339
K-Akira928 <K.AkiraRei928@gmail.com>
2024-04-30 13:14:56 +0900

    feat: index adjust

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,1 +1,39 @@
-商品一覧
+<x-app-layout>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      ホーム
+    </h2>
+  </x-slot>
+
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900">
+          <div class="py-2">
+            <x-flash-message status="session('status')" />
+          </div>
+          <div class="flex justify-end mb-4">
+            <button onclick="location.href='{{ route('owner.products.create') }}'"
+              class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-sm">新規登録</button>
+          </div>
+          <div class="grid grid-cols-4 gap-6">
+            @foreach ($products as $product)
+              <div>
+                <a href="#">
+                  <div class="border rounded-md pt-4 px-4 text-center">
+                    <x-thumbnail filename="{{ $product->imageFirst->filename ?? '' }}" type="products" />
+                    <div class="text-gray-700">
+                    </div>
+                    <div class="py-4">
+                      <span class="text-gray-700">{{ $product->name }}</span>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            @endforeach
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</x-app-layout>
